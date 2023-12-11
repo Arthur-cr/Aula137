40f8d4cf62d8a72d95181b188666c60c744f099a
Arthur <arthurcaetano.acrr@gmail.com>
2023-12-11 12:06:32 -0300

    Aula 135-136

---
+++ Principal/Entities/SavingsAccount.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Principal.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InterestingRate { get; set; }
+        
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestingRate) : base(number, holder, balance)
+        {
+            InterestingRate = interestingRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance = Balance * InterestingRate;
+        }
+    }
+}
